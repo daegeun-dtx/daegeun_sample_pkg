8030f7cf79130a717e78b090b1d4cc98c8d480e2
daegeun-dtx <daegeun.lee@salphadtx.com>
2024-04-30 16:22:16 +0900

    Add: add example widget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:daegeun_sample_pkg/daegeun_sample_pkg.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -17,12 +18,16 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
+  String _localTimeZone = "Unknown";
+  List<String>? _availableTimeZones = [];
   final _daegeunSamplePkgPlugin = DaegeunSamplePkg();
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    initLocalTimezoneState();
+    initAvailableTimeZonesState();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -31,8 +36,8 @@ class _MyAppState extends State<MyApp> {
     // Platform messages may fail, so we use a try/catch PlatformException.
     // We also handle the message potentially returning null.
     try {
-      platformVersion =
-          await _daegeunSamplePkgPlugin.getPlatformVersion() ?? 'Unknown platform version';
+      platformVersion = await _daegeunSamplePkgPlugin.getPlatformVersion() ??
+          'Unknown platform version';
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
@@ -47,6 +52,38 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<void> initLocalTimezoneState() async {
+    String localTimeZone;
+    try {
+      localTimeZone = await _daegeunSamplePkgPlugin.getLocalTimeZone() ??
+          "Unknown local timezone";
+    } on PlatformException {
+      localTimeZone = 'Failed to get local timezone.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _localTimeZone = localTimeZone;
+    });
+  }
+
+  Future<void> initAvailableTimeZonesState() async {
+    List<String> availableTimeZones;
+    try {
+      availableTimeZones =
+          await _daegeunSamplePkgPlugin.getAvailableTimeZones() ?? [];
+    } on PlatformException {
+      availableTimeZones = ['Failed to get available timezones.'];
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _availableTimeZones = availableTimeZones;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -54,8 +91,15 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Running on: $_platformVersion\n'),
+              Text('Local TimeZone: $_localTimeZone\n'),
+              Text('Available TimeZones: $_availableTimeZones\n')
+            ],
+          ),
         ),
       ),
     );
